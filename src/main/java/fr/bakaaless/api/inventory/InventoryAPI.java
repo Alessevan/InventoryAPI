b4518f7c389147cc9b5e9727fbb3f246fa98e58f
SkyCraft78 <50667548+SkyCraft78@users.noreply.github.com>
2021-04-25 19:17:46 +0200

    ✨ Added click function consumer (#16)

--- src/main/java/fr/bakaaless/api/inventory/InventoryAPI.java
+++ src/main/java/fr/bakaaless/api/inventory/InventoryAPI.java
@@ -33,6 +33,7 @@ public class InventoryAPI implements Listener {
     private List<ItemAPI> items;
     private Consumer<InventoryAPI> function;
     private Consumer<InventoryCloseEvent> closeEvent;
+    private Consumer<InventoryClickEvent> clickEvent;
     private boolean interactionCancel;
     private boolean refreshed;
     private boolean build;
@@ -163,6 +164,16 @@ public class InventoryAPI implements Listener {
      */
     public InventoryAPI setCloseFunction(final Consumer<InventoryCloseEvent> function) {
         this.closeEvent = function;
+        return this;
+    }
+
+    /**
+     * Define the click function.
+     * @param function A consumer, that will be triggered when the player will click in the inventory.
+     * @return Your InventoryAPI object
+     */
+    public InventoryAPI setClickFunction(final Consumer<InventoryClickEvent> function) {
+        this.clickEvent = function;
         return this;
     }
 
@@ -477,13 +488,17 @@ public class InventoryAPI implements Listener {
 
     @EventHandler
     public void onClose(final InventoryCloseEvent e) {
-        if (e.getView().getTopInventory().equals(this.inventory))
+        if (e.getView().getTopInventory().equals(this.inventory)) {
+            if (this.closeEvent != null)
+                this.closeEvent.accept(e);
             this.stop();
+        }
         if (!e.getInventory().equals(this.inventory))
             return;
         if (e.getInventory().getHolder() == null) {
+            if (this.closeEvent != null)
+                this.closeEvent.accept(e);
             this.stop();
-            this.closeEvent.accept(e);
         }
     }
 
@@ -493,6 +508,8 @@ public class InventoryAPI implements Listener {
             return;
         if (!e.getClickedInventory().equals(this.inventory))
             return;
+        if (this.clickEvent != null)
+            this.clickEvent.accept(e);
         e.setCancelled(this.interactionCancel);
         final AtomicBoolean slotRegister = new AtomicBoolean(false);
         this.items.forEach(itemAPI -> {
